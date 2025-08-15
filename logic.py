8724649abe7ef30e307acc25b6b1cf6afae00fa9
amonsho <amonsho004@gmail.com>
2025-08-15 14:31:01 +0500

    changed main and logic completely

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 from database import DatabaseConfig
 
-async def create_user(db:DatabaseConfig, user, password_hash):
+async def registration(db:DatabaseConfig, user, password_hash):
    
     try:
         async with db.pool.acquire() as conn:
@@ -12,68 +12,18 @@ async def create_user(db:DatabaseConfig, user, password_hash):
         print('Error',e)
 
 
-async def get_users(db:DatabaseConfig):
+async def login(db:DatabaseConfig,username, password):
     
     try:
         async with db.pool.acquire() as conn:
-            users = await conn.fetch("""
-        SELECT * FROM users;
-    """)
-            return users
+            user = await conn.fetchrow("""
+        SELECT id FROM users WHERE username = $1 and password = $2
+    """,username, password)
+            return user if user else None
+        
     except Exception as e:
         print('Error',e)
 
-async def update_user(db:DatabaseConfig, user_id:int, new_username, new_password):
-    
-    try:
-        async with db.pool.acquire() as conn:
-            if new_username and new_password:
-                await conn.execute("""
-                    UPDATE users
-                    SET username = $1, password = $2
-                    WHERE id = $3
-                """, new_username, new_password, user_id)
-            elif new_username:
-                await conn.execute("""
-                    UPDATE users 
-                    SET username = $1
-                    WHERE id = $2
-                """,new_username,user_id)
-            elif new_password:
-                await conn.execute("""
-                    UPDATE users
-                    SET password = $1
-                    WHERE id = $2
-                """, new_password, user_id)
-            else:
-                print('nothing update')
-    except Exception as e:
-        print('Error', e)
-
-
-async def get_user_by_id(db:DatabaseConfig, user_id):
-    
-    try:
-        async with db.pool.acquire() as conn:
-            user = await conn.fetch("""
-                SELECT * FROM users
-                WHERE id = $1
-        """,user_id)
-            return user
-    except Exception as e:
-        print('Error', e)
-
-async def delete_user(db:DatabaseConfig, user_id):
-    
-    try:
-        async with db.pool.acquire() as conn:
-            result = await conn.execute("""
-                    DELETE FROM users 
-                    WHERE id = $1
-        """,user_id)
-        return result
-    except Exception as e:
-        print('Error',e)
 
 async def create_task(db:DatabaseConfig, title:str, description:str, user_id:str):
     
@@ -85,3 +35,65 @@ async def create_task(db:DatabaseConfig, title:str, description:str, user_id:str
         """, title, description, user_id)
     except Exception as e:
         print('Error:',e)
+
+
+async def get_tasks(db:DatabaseConfig, user_id):
+    
+    try:
+        async with db.pool.acquire() as conn:
+            tasks = await conn.fetch("""
+        SELECT * FROM todos WHERE user_id = $1; 
+        """,user_id)
+            return tasks
+        
+    except Exception as e:
+        print('Error', e)
+
+
+async def update_task(db:DatabaseConfig, task_id:int, title:str, description: str):
+    try:
+        async with db.pool.acquire() as conn:
+            await conn.execute("""
+    UPDATE todos SET title=$1, description=$2 WHERE id=$3
+""",title, description, int(task_id))
+            print('Task updated!')
+    
+    except Exception as e:
+        print('Error:', e)
+
+
+async def delete_task(db:DatabaseConfig, task_id:int):
+    try:
+        async with db.pool.acquire() as conn:
+            await conn.execute("""
+    DELETE FROM todos WHERE id=$1
+""", int(task_id))
+        print('Task deleted!')
+
+    except Exception as e:
+        print('Error:',e)  
+
+
+
+async def get_task_by_id(db:DatabaseConfig, task_id:int):
+    try:
+        async with db.pool.acquire() as conn:
+            await conn.execute("""
+    SELECT * FROM todos WHERE id=$1
+""", int(task_id))
+            
+    except Exception as e:
+        print('Error',e)
+
+# async def delete_user(db:DatabaseConfig, user_id):
+    
+#     try:
+#         async with db.pool.acquire() as conn:
+#             result = await conn.execute("""
+#                     DELETE FROM users 
+#                     WHERE id = $1
+#         """,user_id)
+#         return result
+#     except Exception as e:
+#         print('Error',e)
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 
-from logic import create_user, get_users,update_user,get_user_by_id,delete_user,create_task
+from logic import registration, login, create_task,get_tasks,update_task,delete_task,get_task_by_id
 from database import DatabaseConfig
+
+login_status = False
 
 async def main():
     db=DatabaseConfig(
@@ -12,47 +14,74 @@ async def main():
     await db.connect()
     await db.create_table()
 
-    print("""
-Select action:
-1.Create User
-2.Get Users
-3.Get Users by ID
-4.Update Users
-5.Delete USers
-6.Create task
-""")
+    while True:
+
+        print("""
+    1. Registration 
+    2. Login
+    """)
     
-    choice = input('Select your choice: ')
+        choice = input('Choose one: ')
 
-    if choice == '1':
-        username = input('Enter your username: ')
-        password = input('Enter your password: ')
-        await create_user(db, username, password)
-        print('User is created succsessfully')
+        if choice == '1':
+            username = input('Enter your username: ')
+            password = input('Enter your password: ')
+            await registration(db, username, password)
+            print('User is created succsessfully')
 
-    elif choice == '2':
-        users = await get_users(db)
-        print(users)
+        elif choice == '2':
+            username = input('Enter your username: ')
+            password = input('Enter your password: ')
+            logged_user_id = await login(db, username, password)
+            if logged_user_id:
+                global login_status
+                login_status = True
+        
+        while login_status:
+            print("""
+    1. Add task
+    2. Get all task
+    3. Update task
+    4. Delete task
+    5. Logout
+    6. Get task by id
+""")
+            choice = input('Choose one: ')
 
-    elif choice == '3':
-        user_id = int(input('Enter User ID: '))
-        user = await get_user_by_id(db,user_id)
-        print(user)
+            if choice == '1':
+                title = input('Enter your title: ')
+                description = input('Enter your description: ')
+                await create_task(db, title, description, logged_user_id['id'])
+            
+            elif choice == '2':
+                tasks = await get_tasks(db, logged_user_id['id'])
+                for i, task in enumerate(tasks, 1):
+                    print(f'(id: {task['id']}) {i}. {task['title']} - {task['description']}')
+            
+            elif choice == '3':
+                task_id = input('Enter task id: ')
+                title = input('Enter your new title: ')
+                description = input('Enter your new description: ')
+                await update_task(db, task_id, title, description)
 
-    elif choice == '4':
-        user_id = int(input('Enter USer ID: '))
-        new_username = input('Enter new username: ')
-        new_password = input('Enter new password: ')
-        await update_user(db,user_id, new_username, new_password)
-        print('User update successfully')
+            elif choice == '4':
+                task_id = input('Enter task id: ')
+                await delete_task(db, task_id)
+                print('Task deleted!')
 
-    elif choice == '5':
-        user_id = int(input('Enter ID to delete: '))
-        result= await delete_user(db,user_id)
-        print(result)
+            elif choice == '5':
+                print('You logged out!')
+                login_status = False
 
-    elif choice == '6':
-        await create_task(db, 'Go to work', 'I Need Work',7)
+            elif choice == '6':
+                task_id = input('Enter task id: ')
+                task = await get_task_by_id(db, task_id)
+                if task:
+                    print(f'Task found: {task['title']} - {task['description']}')
+                else:
+                    print('Task not found!')
+
+
 
 if __name__ == '__main__':
     asyncio.run(main())
